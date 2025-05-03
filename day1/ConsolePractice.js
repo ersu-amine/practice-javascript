fec6ac797863be10080c32f02f08d6a36e2abf02
ersu-amine <ersu.amine@gmail.com>
2025-05-03 01:38:33 -0400

    practice console.log to print variables in a sentence

---
+++ day1/ConsolePractice.js
@@ -0,0 +1,17 @@
+//Day 1 – Console & Basics
+/*
+Challenge: Write a program that logs your name, your favorite color, and your favorite programming language in one sentence using console.log.
+*/
+
+const myName = "Sora";
+const favDrink = "coffee";
+
+//using ` instead of "" to utilize ${variable}, instead of concatenation
+console.log(`My name is ${myName} and I love ${favDrink}.`);
+
+//same as, it will add new line
+// System.out.println("") for java
+
+// System.out.print("") does not exist for js, as google say
+
+
