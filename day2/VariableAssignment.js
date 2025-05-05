8ee8a9830b4e2611b540b149c74c045feb221b70
ersu-amine <ersu.amine@gmail.com>
2025-05-05 18:40:45 -0400

    Day 2 exercises

---
+++ day2/VariableAssignment.js
@@ -0,0 +1,8 @@
+/*
+Day 2 – Variables & Data Types
+Challenge 2: Try changing the value of a const variable and observe the error.
+*/
+
+const favDrink = "coffee";
+
+favDrink = "tea"; //getting type error because variable is constant and cannot be changed
---
+++ day2/Variables.js
@@ -0,0 +1,10 @@
+/*
+Day 2 – Variables & Data Types
+Challenge 1: Create variables for your name, age, and whether you're a student. Print them all in a meaningful sentence.
+*/
+
+const myName = "Jessica";
+const myAge = 33;
+let isStudent = false;
+
+console.log(`I am ${myName} and I'm ${myAge} years old. My student status is:${isStudent}`)
