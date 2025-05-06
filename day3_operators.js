4554d7c0b4f756aa228c9a3134fd2da14e1a0111
ersu-amine <ersu.amine@gmail.com>
2025-05-06 00:44:46 -0400

    Day3 practice

---
+++ day3_operators.js
@@ -0,0 +1,11 @@
+// Day 3: Operators
+let item1 = 10, item2 = 20, item3 = 30;
+let total = item1 + item2 + item3;
+let taxRate = 0.08;
+let totalWithTax = total * (1 + taxRate);
+console.log(`Total with tax: ${totalWithTax}`);
+
+let a = 5, b = 10;
+console.log(a > b);
+console.log(a < b);
+console.log(a === b);
