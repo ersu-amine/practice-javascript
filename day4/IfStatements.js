00ff028c3dc2fb64e52e4a56b46cb95e1b579efe
ersu-amine <ersu.amine@gmail.com>
2025-05-08 00:05:25 -0400

    Day 4 practice

---
+++ day4/IfStatements.js
@@ -0,0 +1,11 @@
+// Day 4: Conditional Statements
+//should implement user input like scanner in java
+let word = "My favourite apples are yellow in colour.";
+let isApple = false;
+if (word.includes("apple")) {
+    isApple = true;
+    console.log(isApple);
+} else {
+    console.log("The word has no " + word)
+}
+
---
+++ day4/SwtichStatements.js
@@ -0,0 +1,17 @@
+// Day 4: Conditional Statements
+
+//numbers have rating , 3 is escellent and 1 is good
+let number = 2;
+switch (number) {
+    case 1:
+        console.log("Good");
+        break;
+    case 2:
+        console.log("Very good");
+        break;
+    case 3:
+        console.log("Excellent");
+        break;
+
+
+}
